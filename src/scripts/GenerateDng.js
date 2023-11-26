581ba389b0834335eb370b44eca551cc52750d2a
Jack Darker <JackDarker@freenet.de>
2023-11-26 17:52:28 +0100

    fix Gen

--- src/scripts/GenerateDng.js
+++ src/scripts/GenerateDng.js
@@ -35,15 +35,17 @@
         constructor(){
         }
         //resets state of generator
-        reset(params){
+        reset(){
             this.length=1;
-            this.room = new Room();
-            this.grid = new Map([[this.room.getAddress(),this.room]]);
-            this.mainrooms = [this.room.getAddress()];
+            this.minX=0,this.minY=0;
+            this.mainrooms = [];
             this.doors =[]; //[ [ [1,1],[0,1] ],... ]
+            this.room = new Room();
+            this.grid = new Map();
+            this.placeRoom(this.room);            
         };
-        intersects(newroom){
-            let inter=[],rooms = Array.from(this.grid.values()),newtiles=newroom.tiles;
+        intersects(newRoom){
+            let inter=[],rooms = Array.from(this.grid.values()),newtiles=newRoom.tiles;
             for(var i=rooms.length-1;i>=0;i--){
                 let tiles=rooms[i].tiles;
                 for(var k =newtiles.length-1;k>=0;k--){
@@ -58,6 +60,14 @@
         }
         placeRoom(newRoom){
             this.grid.set(newRoom.getAddress(),newRoom);
+            for(var k=newRoom.tiles.length-1;k>=0;k--){//add doors to templates
+                this.minX=Math.min(this.minX,newRoom.tiles[k][0]),this.minY=Math.min(this.minY,newRoom.tiles[k][1]);
+                for(var l=newRoom.tiles.length-1;l>=0;l--){
+                    if( this.isNeighbour(newRoom.tiles[k],newRoom.tiles[l])){ //tiles directly beside each other?
+                        this.doors.push([newRoom.tiles[k],newRoom.tiles[l]]);
+                    }
+                }
+            }
             this.mainrooms.push(newRoom.getAddress());
         }
         isNeighbour(tileA,tileB){
@@ -76,6 +86,19 @@
                 _sane.push(y);
             }
         }
+        toCoord(tile){ 
+            const X=['A','B','C','D','E','F','G','H','I','J','K','L','M','N'];
+            const Y=['0','1','2','3','4','5','6','7','8','9'];
+            switch(this.params.naming){
+                case 'A1': return(X[tile[0]-this.minX]+Y[tile[1]-this.minY]); //returns 'E5', also shifts Position as smallest value is A1
+                break;
+                case '-12_1': return((tile[0]-this.minX)+'_'+(tile[1]-this.minY));
+                break;
+                case '-12_+01': return(window.gm.util.formatInt(tile[0]-this.minX,true,2)+
+                    '_'+window.gm.util.formatInt(tile[1]-this.minY,true,2));
+                break;
+            }
+        } 
         /**
          *  generates new map and returns it
          * @memberof Generator
@@ -88,36 +111,51 @@
          * branches: defines if mainrooms can branch out: default 0; 2 means there could be 2 forks
          */
         generate(params){
-            let _params=params||{};
-            _params.length = (_params.length)?_params.length:4;
-            _params.doors = (_params.doors)?_params.doors:0;
-            _params.naming = (_params.naming)?_params.naming:'A1';
-            _params.branches = (_params.branches)?_params.branches:0;
-            _params.progress = (_params.progress)?_params.progress:function(){};
-            this.reset(_params);
-            let pos,n,minX=0,minY=0;
+            this.params=params||{};
+            this.params.length = (this.params.length)?this.params.length:4;
+            this.params.doors = (this.params.doors)?this.params.doors:0;
+            this.params.naming = (this.params.naming)?this.params.naming:'A1';
+            this.params.branches = (this.params.branches)?this.params.branches:0;
+            this.params.progress = (this.params.progress)?this.params.progress:function(){};
+            this.reset();
+            let pos,n,branched=0;
             let slots =[[-2,-1],[2,1],[-2,1],[2,-1],[-1,-2],[1,-2],[-1,2],[1,2]];
-            while(this.length<_params.length){
+            while(this.length<this.params.length){
                 n = Array.from(slots); 
                 n.sort(() => Math.random() - 0.5);// scramble array
-                let newroom= new Room();
+                let newRoom= new Room();
                 while(true){
                     pos = n.pop();
-                    newroom.setPosition([pos[0]+this.room.pos[0],pos[1]+this.room.pos[1]]);
-                    //check if newroom would intersect with present rooms
-                    if(!this.intersects(newroom)) {
-                        this.placeRoom(newroom);
+                    newRoom.setPosition([pos[0]+this.room.pos[0],pos[1]+this.room.pos[1]]);
+                    //check if newRoom would intersect with present rooms
+                    if(!this.intersects(newRoom)) {
+                        this.placeRoom(newRoom);
+                        //find neigboring tiles of new and previous room and build main doors 
+                        var brk=false;
+                        for(var k=newRoom.tiles.length-1;k>=0 && brk===false ;k--){
+                            for(var l=this.room.tiles.length-1;l>=0 && brk===false;l--){
+                                if( this.isNeighbour(newRoom.tiles[k],this.room.tiles[l])){ //tiles directly beside each other?
+                                    this.doors.push([newRoom.tiles[k],this.room.tiles[l]]);
+                                    brk=true;
+                                }
+                            }
+                        }
                         this.length+=1;
-                        //choose next room adjoining current
                         //todo create branch
                         //after adding a room, there is a chance to select a previous mainroom to continue on instead of extending from current mainroom
-                        this.room = newroom;
+                        if(branched<this.params.branches) {
+                            var x = Math.round((this.params.length-1)*Math.random()); //likelyness to branch should increase with length
+                            if(x<(this.mainrooms.length-1)) {
+                                this.room = this.grid.get(this.mainrooms[x]);
+                                branched+=1;
+                            } else this.room = newRoom;
+                        } else this.room = newRoom; //memorize for next iter
                         break;
                     }
                 }
             }
             //add doors to templates
-            for(var i=this.mainrooms.length-1;i>=0;i--){
+            /*for(var i=this.mainrooms.length-1;i>=0;i--){
                 let ra=this.grid.get(this.mainrooms[i]);
                 for(var k=ra.tiles.length-1;k>=0;k--){
                     minX=Math.min(minX,ra.tiles[k][0]),minY=Math.min(minY,ra.tiles[k][1]);
@@ -140,7 +178,7 @@
                         }
                     }
                 }
-            }
+            }*/
             
             let _allrooms = [];
             for (let x of this.grid.values()){
@@ -151,7 +189,7 @@
             //build additional doors between templates
             for(var i=1;i<this.mainrooms.length;i++){//walk each mainroom
                 let brk,ra=this.grid.get(this.mainrooms[i]);
-                let n=_params.doors,tiles= ra.tiles;
+                let n=this.params.doors,tiles= ra.tiles;
                 for (let x of _allrooms.values()){
                     for(var k=tiles.length-1; k>=0 && n>0;k--){
                         //select a random tile and check neighbours from other mainrooms
@@ -174,31 +212,18 @@
             //{grid:new Map(_grid.map((x)=>{return([x.room,x]);})),width:14,height:8,legend:''}
             //where x= {room:'D2', dirs:[{dir:'E2'}]},
             let _grid=[];
-            let X=['A','B','C','D','E','F','G','H','I','J','K','L','M','N'],Y=['0','1','2','3','4','5','6','7','8','9'];
-            function toCoord(tile){ 
-                switch(_params.naming){
-                    case 'A1': return(X[tile[0]-minX]+Y[tile[1]-minY]); //returns 'E5', also shifts Position as smallest value is A1
-                    break;
-                    case '-12_1': return((tile[0]-minX)+'_'+(tile[1]-minY));
-                    break;
-                    case '-12_+01': return(window.gm.util.formatInt(tile[0]-minX,true,2)+
-                        '_'+window.gm.util.formatInt(tile[1]-minY,true,2));
-                    break;
-                }
-                 
-            } 
             for (let x of this.grid.values()){
                 let pos,dirs;
                 for(var i=x.tiles.length-1;i>=0;i--){
                     dirs=[]; //doors defined for this tile?
                     for(var k=this.doors.length-1;k>=0;k--){
                         if(this.doors[k][0]==x.tiles[i] ) {
-                            dirs.push({dir:toCoord(this.doors[k][1])});
+                            dirs.push({dir:this.toCoord(this.doors[k][1])});
                         } else if(this.doors[k][1]==x.tiles[i] ) {
-                            dirs.push({dir:toCoord(this.doors[k][0])});
+                            dirs.push({dir:this.toCoord(this.doors[k][0])});
                         }
                     }
-                    pos=toCoord(x.tiles[i]);
+                    pos=this.toCoord(x.tiles[i]);
                     _grid.push({room:pos,dirs:dirs})
                 }
             }
